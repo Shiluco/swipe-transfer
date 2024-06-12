d9531498a7b22c9ce424943b94f955c024e21d13
Shiluco <hirap1939@gmail.com>
2024-06-12 09:57:51 +0900

    fix:little looks bug

--- src/components/OneStation.tsx
+++ src/components/OneStation.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import { DndContext } from "@dnd-kit/core";
 import GetStationItem from "./Draggable/DragStationItem";
 import StationDropArea from "./Droppable/StationDropArea";
